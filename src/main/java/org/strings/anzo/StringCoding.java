359c1899d784d10bb1cb9938d395468dc32cf95d
Anzo85 <zolotarev@3g.ua>
2016-09-11 14:17:07 +0300

    Update StringCoding.java

--- src/main/java/org/strings/anzo/StringCoding.java
+++ src/main/java/org/strings/anzo/StringCoding.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by andreyz on 10.09.16.
- */
 public class StringCoding {
 
     public static void main(String[] args) throws IOException {
@@ -23,10 +20,8 @@ public class StringCoding {
     public static String coding(String s) throws IOException {
 
         String y = s.substring(1, s.length() - 1);
-
-        StringBuffer sb = new StringBuffer(s);
-        char chFirst = sb.charAt(0);
-        char chLast = sb.charAt(sb.length() - 1);
+        char chFirst = s.charAt(0);
+        char chLast = s.charAt(s.length() - 1);
         int index = y.length();
         String result = (chFirst + "" + index + chLast);
         return result;
